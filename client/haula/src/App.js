60d850dcc55dd0b146a12ad57ce0c88df7368ed1
RICGRO1221 <ldg.ricardo@gmail.com>
2018-04-17 20:21:09 -0700

    All the good stuff

--- client/haula/src/App.js
+++ client/haula/src/App.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import AppBarExampleIcon from './components/nav';
+// import AppBarExampleIcon from './components/nav';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  PrivateRoute,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+// const Router = ReactRouter.BrowserRouter;
+// const Route = ReactRouter.Route;
+// const Switch = ReactRouter.Switch;
+import Home from './components/Home';
+import Customer from './components/Customer';
+import Mover from './components/Mover';
+import SignUp from './components/SignUp';
+import Nav from './components/Nav';
+import Login from './components/Login';
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <AppBarExampleIcon/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+          <div className='container'>
+            <Nav />
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/SignUp' component={SignUp} /> 
+                <Route exact path='/Login' component={Login} />
+                <Route exact path='/Customer' component={Customer} />
+                <Route exact path='/Mover' component={Mover} />
+                <Route render= {() => <p>Page Not Found 404</p>} />  
+            </Switch>
+          </div>
+      </Router>
+    )
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
--- client/haula/src/components/nav/nav.js
+++ client/haula/src/components/nav/nav.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import AppBar from 'material-ui/AppBar';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import { green500 } from 'material-ui/styles/colors';
+const React = require('react');
+//Link to Router...
+// import AppBar from 'material-ui/AppBar';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// import { green500 } from 'material-ui/styles/colors';
+const NavLink = require('react-router-dom').NavLink;
 
-const muiTheme = getMuiTheme({
-    appBar: {
-        height: 500,
-        color: green500,
-    },
-});
 
-const AppBarExampleIcon = () => (
-  <MuiThemeProvider muiTheme={muiTheme}>
-  <AppBar
-    title="Title"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  />
-  </MuiThemeProvider>
-);
 
-export default AppBarExampleIcon;
+function Nav () {
+  return (
+    <ul className="nav">
+      <li>
+        <NavLink exact activeClassName='active' to='/'>
+          Home//This is for the title Logo
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/SignUp'>
+          Sign up
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/Login'>
+          Login
+        </NavLink>
+      </li>
+    </ul>
+  )
+}
+
+module.exports = Nav;
