000110f7cc16724c49b88f27233925f97708ae0f
William Lees <william@Williams-MacBook-Air.local>
2018-04-21 14:40:14 -0700

    master

--- client/haula/src/utils/api.js
+++ client/haula/src/utils/api.js
@@ -8,18 +8,6 @@ export default {
 
   getOrdersPending : function() {
       return axios.get("/api/orders/pending");
-  },
-
-  signup: function () {
-      return axios.post("/signup");
-  },
-
-  login: function () {
-      return axios.get("/signin");
-  },
-
-  signout: function () {
-      return axios.get("/signout");
   }
  
 };
