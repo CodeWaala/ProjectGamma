ffb223912ba6ca8a508b693eb74b6e9156495efa
CodeWaala <abhishek.purdue@gmail.com>
2018-04-21 10:14:55 -0700

    footer, mover, home

--- client/haula/src/Pages/Home/home.js
+++ client/haula/src/Pages/Home/home.js
@@ -69,7 +69,6 @@ export class Home extends Component {
                         </Parallax.Layer>
 
                     </Parallax>
-        <Footer/>
           {/* <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
