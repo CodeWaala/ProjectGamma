09a912fb1b2da60abd9ae5ca44a09e6e37676eab
RICGRO1221 <ldg.ricardo@gmail.com>
2018-04-24 21:33:34 -0700

    commit Ui Homepage

--- client/haula/src/Pages/Home/home.js
+++ client/haula/src/Pages/Home/home.js
@@ -5,13 +5,13 @@ import { Image,Button, Grid, Row, Col } from 'react-bootstrap';
 import { Parallax } from 'react-spring'
 import { request } from 'http';
 import './home.css';//
-import background1 from '../../Shared/main_images/main_box1-02.jpg';
+import bg1 from '../../Shared/main_images/main_bg.jpg';
+import boxes from '../../Shared/main_images/main_boxes.png';
 
 
-<img src={background1}/>;
 
-const personHolder = require('./components/images/person-icon.png')
-// const imgSource1 = require("./components/images/background1.jpg")
+const personHolder = require('./components/images/person-icon.png');
+//const imgSource1 = require('./components/images/background1.jpg');
 // const imgSource2 = require("./components/images/background2.jpg")
 const styles = {
     fontFamily: 'Menlo-Regular, Menlo, monospace',
@@ -25,9 +25,16 @@ export class Home extends Component {
     render() {
       return (
         <div className="App">
-                <div className='parallax img1'>
+                <div className='parallax bg1' style={{backgroundColor:"#50dfbf"}}>
+                    <div>
+                        <img src={bg1} className='boxes'/>;
+                    </div>
+                    <Button className="customer-btn">Customer</Button>
+                    <Button className="mover-btn">Mover</Button>
+
                 </div>
-                <div style={{height: "500px", backgroundColor: "black",display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
+                
+                <div className='second' style={{height: "500px", backgroundColor: "black", display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
                     <div class="row-container">
                     <Grid>
                         <Row>
