35101229a9ff7b167244b49a62dad79eb6875e38
CodeWaala <abhishek.purdue@gmail.com>
2018-04-25 00:16:27 -0700

    google maps pages

--- client/haula/src/Pages/Mover/movecardcontainer/movecardcontainer.js
+++ client/haula/src/Pages/Mover/movecardcontainer/movecardcontainer.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./movecardcontainer.css";
 
 export const MoveCardContainer = ({ children }) => {
-    console.log(children)
     return <div className="flex-1">{children}</div>
 };
 
--- client/haula/src/Pages/Mover/mover.js
+++ client/haula/src/Pages/Mover/mover.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import { MyNavbar } from "../../Shared/nav/nav2";
 import { Footer } from "../../Shared/footer/footer";
-import { Image, Grid, Row, Col,Thumbnail, Button, Glyphicon } from "react-bootstrap";
+import {
+  Image,
+  Grid,
+  Row,
+  Col,
+  Thumbnail,
+  Button,
+  Glyphicon
+} from "react-bootstrap";
 import MapWithAMarker from "../../Shared/googlemap/MapContainer";
-import './mover.css'
+import "./mover.css";
 
 //import { Mover } from './Pages/Mover/mover';
-import {MoveCardContainer} from "./movecardcontainer/movecardcontainer.js";
+import { MoveCardContainer } from "./movecardcontainer/movecardcontainer.js";
 import MoverCard from "./movecard/movercard";
 import API from "../../utils/api";
 import orders from "./orders.json";
 const google = window.google;
 const navigator = window.navigator;
+let geocoder = new window.google.maps.Geocoder();
 
 export class Mover extends Component {
   constructor(props) {
@@ -27,58 +36,84 @@ export class Mover extends Component {
       FurnitureImage: "images/furniture-picture.jpg",
       moverequests: [],
       latlongs: [],
-      lat : null,
-      lng : null
+      lat: null,
+      lng: null
     };
   }
 
-    componentDidMount() {
-        this.getMovesInfo();
-        this.getlatlng();
-    }
+  componentDidMount() {
+    this.getMovesInfo();
+    
+   
+  }
 
-    getMovesInfo = () => {
-        //api call to get data from DB (moves or order information)
-        API.getOrders()
-         .then(res => this.setState({moverequests: res.data}))
-         .catch(err => console.log(err));
-    }
+  getMovesInfo = () => {
+    //api call to get data from DB (moves or order information)
+    API.getOrders()
+      .then(res => { 
+        this.setState({ moverequests: res.data })
+        this.getlatlngs(res.data);
+        this.getCurrentlatlng();
+      })
+      .catch(err => console.log(err));
+  };
 
-    getlatlng() {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          this.setState({ lat: position.coords.latitude, lng: position.coords.longitude});
-          alert(position.coords.latitude);
-        },
-        error => console.log(error)
-      );
-    }
+  getCurrentlatlng() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+        //alert(position.coords.latitude);
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
+  getlatlngs(requests) {
+    {this.state.moverequests.map((request,i) => (
+      geocoder.geocode({ 'address': request.fromaddress }, function(results, status) {
+        if (status == "OK") {
+          var coords = results[0].geometry.location.toJSON();
+          console.log(coords);
+          this.setState({latlongs: coords })
+        } else {
+          console.log(
+            "Geocode was not successful for the following reason: " + status
+          );
+        }
+      })
+    ))} 
+  }
 
   render() {
     return (
       <div className="flex-box">
         <div className="flex-1">
-        {console.log(this.state.moverequests)}
           <MoveCardContainer>
-          {
-            this.state.moverequests.map((request, i) => (
-            <MoverCard
-              key={request.key}
-              price={request.expectedprice}
-              toaddress={request.toaddress}
-              fromaddress={request.fromaddress}
-            />
-          ))}
-        </MoveCardContainer>
+            {this.state.moverequests.map((request, i) => (
+              <MoverCard
+                key={request.key}
+                price={request.expectedprice}
+                toaddress={request.toaddress}
+                fromaddress={request.fromaddress}
+              />
+            ))}
+          </MoveCardContainer>
         </div>
         <div className="flex-2">
-        <MapWithAMarker
+            <MapWithAMarker
               googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDW2jclEKZ5j-zWenaspVMvdBekq60o1TQ&libraries=geometry,drawing,places"
               loadingElement={<div style={{ height: `100%` }} />}
               containerElement={<div style={{ height: `400px` }} />}
               mapElement={<div style={{ height: `100%` }} />}
-              test="test"
-            /> 
+              requests={this.state.latlongs}
+              currentlat={this.state.lat}
+              currentlng={this.state.lng}
+            />
         </div>
       </div>
     );
